cab6e21e9bdda89a24a30b6cd867366358c581bf
phageghost <github@phageghost.net>
2018-08-27 18:01:46 -0700

    Piecewise linear working well, in progress on power function

--- peas/fitapproxdistros/distributions.py
+++ peas/fitapproxdistros/distributions.py
@@ -1,7 +1,7 @@
 import numpy
 import scipy.stats
 
-from empdist.empirical_pval import compute_empirical_pvalue
+from empdist.empirical_pval import compute_empirical_pvalue,  determine_p_cutoff, compute_empirical_quantile
 from scipy.optimize import curve_fit
 
 DEFAULT_MAX_EMPIRICAL_SIZE = 50
@@ -28,26 +28,68 @@ class PiecewiseApproxLinear:
         return numpy.piecewise(x, [x < inflection_point], [lambda x: 0, lambda x: slope * (x - inflection_point)])
 
     @classmethod
-    def fit(cls, data, is_sorted=False, support_quantile=DEFAULT_SUPPORT_QUANTILE, interp_points=50,
+    def fit(cls, data, is_sorted=False, max_pvalue_std_error=0.05, interp_points=50,
             initial_inflection_point=None, initial_slope=500):
         if not is_sorted:
             data = numpy.sort(data)
 
-        data_min, data_max = data[0], data[-1]
-        extent = data_max - data_min
-        midpoint = (data_min + data_max) / 2
+        min_val, max_val = data.min(), data.max()
+        data_mean = data.mean()
+        endpoint = compute_empirical_quantile(data, 1 - determine_p_cutoff(n=len(data)), is_sorted=True)
 
-        support = midpoint - extent, midpoint + (extent / 2) * support_quantile
+        if endpoint <= data_mean:
+            raise ValueError(
+                'Minimum data value that meets target p-value error threshold of {} is {}, which is below the mean of {}, therefore piecewise linear approximation will not work. Try generating more empirical samples or increasing the error tolerance.'.format(
+                    max_pvalue_std_error, endpoint, initial_inflection_point))
+
+        fit_xs = numpy.concatenate((numpy.linspace(min_val, data_mean, num=interp_points),
+                                    numpy.linspace(data_mean, endpoint, num=interp_points)))
+        fit_ys = numpy.log(compute_empirical_pvalue(data, values=fit_xs, tail='right', is_sorted=True))
 
         if initial_inflection_point == None:
-            initial_inflection_point = data.mean()
-        # print('min: {} max: {} midpoint: {} extent: {} support: {}'.format(data_min, data_max, midpoint, extent, support))
+            initial_inflection_point = data_mean
 
-        fit_xs = numpy.linspace(*support, num=interp_points)
-        fit_ys = numpy.log(compute_empirical_pvalue(data, values=fit_xs, tail='right', is_sorted=True))
         p, e = scipy.optimize.curve_fit(cls._piecewise_logsf, fit_xs, fit_ys,
                                         p0=[initial_inflection_point, initial_slope])
         return p
+
+    def logsf(self, x):
+        return self._piecewise_logsf(x, self.inflection_point, self.slope)
+
+
+class PiecewiseApproxLinearDirect:
+    """
+    Stub class for an empirical distribution with methods to:
+        1. Fit a piecewise linear function to the log-survival function of a data sample
+        2. Compute the value of the log-survival function for given x.
+    """
+
+    def __init__(self, inflection_point, slope):
+        self.inflection_point = inflection_point
+        self.slope = slope
+
+    @staticmethod
+    def _piecewise_logsf(x, inflection_point, slope):
+        """
+        A piecewise linear function that = 0 for all x < :param:`inflection_point`
+            and rises linearly with slope :param:`slope` for all points > :param:`inflection_point`
+        """
+        return numpy.piecewise(x, [x < inflection_point], [lambda x: 0, lambda x: slope * (x - inflection_point)])
+
+    @classmethod
+    def fit(cls, data, is_sorted=False, max_pvalue_std_error=0.05):
+        min_val, max_val = data.min(), data.max()
+        data_mean = data.mean()
+        endpoint = compute_empirical_quantile(data, 1 - determine_p_cutoff(n=len(data)), is_sorted=True)
+        inflection_point = data_mean
+        print('Max examined data point: {}'.format(endpoint))
+
+        fit_xs = [inflection_point, endpoint]
+        fit_ys = [0, numpy.log(compute_empirical_pvalue(data, values=endpoint, tail='right', is_sorted=is_sorted))]
+
+        slope = (fit_ys[1] - fit_ys[0]) / (fit_xs[1] - fit_xs[0])
+
+        return inflection_point, slope
 
     def logsf(self, x):
         return self._piecewise_logsf(x, self.inflection_point, self.slope)
