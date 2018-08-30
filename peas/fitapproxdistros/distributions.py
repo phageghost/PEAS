43f0367720f0ff25b1267f5797fa566365344dd5
dskola <git@phageghost.net>
2018-08-29 18:09:07 -0700

    Working on distribution selection method

--- peas/fitapproxdistros/distributions.py
+++ peas/fitapproxdistros/distributions.py
@@ -1,11 +1,33 @@
 import numpy
 import scipy.stats
 
-from empdist.empirical_pval import compute_empirical_pvalue,  determine_p_cutoff, compute_empirical_quantile
+from empdist.empirical_pval import compute_empirical_pvalue, compute_p_confidence, compute_empirical_quantile
 from scipy.optimize import curve_fit
 
 
-class PiecewiseApproxLinear:
+class PiecewiseEmpiricalApprox():
+    @classmethod
+    def _compute_empirical_logsf(cls, data, max_pvalue_std_error=0.05, interp_points=50, is_sorted=False):
+        if not is_sorted:
+            data = numpy.sort(data)
+
+        min_val, max_val = data.min(), data.max()
+        data_mean = data.mean()
+        endpoint = compute_empirical_quantile(data, 1 - compute_p_confidence(n=len(data)), is_sorted=True)
+
+        if endpoint <= data_mean:
+            raise ValueError(
+                'Minimum data value that meets target p-value error threshold of {} is {}, which is below the mean of {}, therefore piecewise approximation will not work. Try generating more empirical samples or increasing the error tolerance.'.format(
+                    max_pvalue_std_error, endpoint, data_mean))
+
+        fit_xs = numpy.concatenate((numpy.linspace(min_val, data_mean, num=interp_points),
+                                    numpy.linspace(data_mean, endpoint, num=interp_points)))
+        fit_ys = numpy.log(compute_empirical_pvalue(data, values=fit_xs, tail='right', is_sorted=True))
+
+        return fit_xs, fit_ys
+
+
+class PiecewiseApproxLinear(PiecewiseEmpiricalApprox):
     """
     Stub class for an empirical distribution with methods to:
         1. Fit a piecewise linear function to the log-survival function of a data sample
@@ -32,7 +54,7 @@ class PiecewiseApproxLinear:
 
         min_val, max_val = data.min(), data.max()
         data_mean = data.mean()
-        endpoint = compute_empirical_quantile(data, 1 - determine_p_cutoff(n=len(data)), is_sorted=True)
+        endpoint = compute_empirical_quantile(data, 1 - compute_p_confidence(n=len(data)), is_sorted=True)
 
         if endpoint <= data_mean:
             raise ValueError(
@@ -54,7 +76,7 @@ class PiecewiseApproxLinear:
         return self._piecewise_logsf(x, self.inflection_point, self.slope)
 
 
-class PiecewiseApproxLinearDirect:
+class PiecewiseApproxLinearDirect(PiecewiseEmpiricalApprox):
     """
     Stub class for an empirical distribution with methods to:
         1. Fit a piecewise linear function to the log-survival function of a data sample
@@ -77,7 +99,7 @@ class PiecewiseApproxLinearDirect:
     def fit(cls, data, is_sorted=False, max_pvalue_std_error=0.05):
         min_val, max_val = data.min(), data.max()
         data_mean = data.mean()
-        endpoint = compute_empirical_quantile(data, 1 - determine_p_cutoff(n=len(data)), is_sorted=True)
+        endpoint = compute_empirical_quantile(data, 1 - compute_p_confidence(n=len(data)), is_sorted=True)
         inflection_point = data_mean
         print('Max examined data point: {}'.format(endpoint))
 
@@ -92,7 +114,7 @@ class PiecewiseApproxLinearDirect:
         return self._piecewise_logsf(x, self.inflection_point, self.slope)
 
 
-class PiecewiseApproxPower:
+class PiecewiseApproxPower(PiecewiseEmpiricalApprox):
     """
     Stub class for an empirical distribution with methods to:
         1. Fit a power function to the log-survival function of a data sample
@@ -116,31 +138,12 @@ class PiecewiseApproxPower:
                                [lambda x: 0, lambda x: scale * (x - inflection_point) ** power])
 
     @classmethod
-    def fit(cls, data, is_sorted=False, max_pvalue_std_error=0.05, interp_points=50,
-            initial_inflection_point=None, initial_power=1, optimization_kwargs={}):
-
-        if not is_sorted:
-            data = numpy.sort(data)
-
-        min_val, max_val = data.min(), data.max()
-        data_mean = data.mean()
-        endpoint = compute_empirical_quantile(data, 1 - determine_p_cutoff(n=len(data)), is_sorted=True)
-
-        if endpoint <= data_mean:
-            raise ValueError(
-                'Minimum data value that meets target p-value error threshold of {} is {}, which is below the mean of {}, therefore piecewise linear approximation will not work. Try generating more empirical samples or increasing the error tolerance.'.format(
-                    max_pvalue_std_error, endpoint, initial_inflection_point))
-
-        fit_xs = numpy.concatenate((numpy.linspace(min_val, data_mean, num=interp_points),
-                                    numpy.linspace(data_mean, endpoint, num=interp_points)))
-        fit_ys = numpy.log(compute_empirical_pvalue(data, values=fit_xs, tail='right', is_sorted=True))
-
-        if initial_inflection_point is None:
-            initial_inflection_point = data_mean
+    def fit_with_existing_empirical_logsf(cls, fit_xs, fit_ys, x0=(None, 1), optimization_kwargs={}):
+        initial_inflection_point, initial_power = x0
 
         res = scipy.optimize.minimize(fun=cls._generate_obj_func(fit_xs, fit_ys),
                                       x0=numpy.array([initial_inflection_point, initial_power]),
-                                      bounds=((-numpy.inf, endpoint - 1e-4),
+                                      bounds=((-numpy.inf, fit_xs[-1] - 1e-4),
                                               (1, numpy.inf)),
                                       method='L-BFGS-B', **optimization_kwargs
                                       )
@@ -152,12 +155,33 @@ class PiecewiseApproxPower:
         return inflection_point, power, scale
 
     @classmethod
+    def fit(cls, data, is_sorted=False, max_pvalue_std_error=0.05, interp_points=50,
+            x0=(None, 1), optimization_kwargs={}):
+
+        initial_inflection_point, initial_power = x0
+
+        if not is_sorted:
+            data = numpy.sort(data)
+
+        fit_xs, fit_ys = cls._compute_empirical_logsf(data, max_pvalue_std_error=max_pvalue_std_error,
+                                                      interp_points=interp_points, is_sorted=True)
+
+        if initial_inflection_point is None:
+            initial_inflection_point = data.mean()
+
+        return cls.fit_with_existing_empirical_logsf(fit_xs, fit_ys, x0=(initial_inflection_point, initial_power),
+                                                     optimization_kwargs=optimization_kwargs)
+
+    @classmethod
     def _generate_obj_func(cls, fit_xs, fit_ys):
         def obj_func(params):
             inflection_point, power = params
             test_ys = cls._piecewise_logsf(x=fit_xs, inflection_point=inflection_point, power=power, scale=-1)
 
             score = scipy.stats.pearsonr(test_ys, fit_ys)[0]
+            # if numpy.isnan(score) or numpy.isinf(score):
+            #     return numpy.inf
+
             return -score
 
         return obj_func
