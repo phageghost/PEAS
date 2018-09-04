38076697de539f991c5fa14884ca2b6fee45669f
phageghost <github@phageghost.net>
2018-09-04 10:05:11 -0700

    Added argument parsing to command-line script

--- peas/genomic_regions.py
+++ peas/genomic_regions.py
@@ -47,3 +47,126 @@ def make_UCSC_bed_file(filename, bed_df, track_name, description=''):
     with open(filename, 'wt') as out_file:
         out_file.write('track name={} description="{}"\n'.format(track_name, description))
         ucsc_formatted_bed_df.to_csv(out_file, sep='\t', header=False, index=False)
+        
+        
+        
+        
+def parse_bed_file(bed_fname, 
+        
+
+        
+        
+        
+def find_vector_crds(input_data, 
+                        annotations,
+                        min_score=None,
+                        max_pval=0.05,
+                        min_size=2, 
+                        max_size=None, 
+                        edge_weight_power=1,
+                        tail='both',
+                        fdr=0.05, 
+                        trim_edges=False, gobig=True, 
+                        num_cores=24,
+                        maximization_target='p_prod',
+                        pvalue_target=DEFAULT_PVALUE_TARGET,
+                        random_seed=None):
+
+    region_dfs = []
+    total_peaks = 0
+    total_regions = 0
+    
+    # Convert data to dict keyed by chrom. Perform correlations if 2D features
+    annotations_by_chrom = {}
+    for chrom, chrom_anno in annotations.groupby('Chr'):
+        annotations_by_chrom[chrom] = chrom_anno.sort_values('Start')
+        
+#     return annotations_by_chrom
+    
+    chrom_data = {}
+    print('*'*80)
+    print('Global namespace: {}'.format(dir()))
+    print('*'*80)
+    
+    if len(input_data.shape) > 1:
+        log_print('Performing pairwise correlations...', 1)
+        generate_correlation_matrices_from_annotated_peaks_multicore(annotations, fname_prefix='', num_cores=num_cores)
+    else:
+        for chrom in annotations_by_chrom:
+            chrom_data[chrom] = input_data.loc[annotations_by_chrom[chrom].index]
+        
+    params = []
+        
+    for chrom in sorted(input_data_by_chrom):
+        chrom = chrom.strip()
+        if chrom is not 'chrM':
+            params.append([chrom,
+                           annotations_by_chrom[chrom], 
+                           min_score,
+                           max_pval,
+                           min_size,
+                           max_size, 
+                           edge_weight_power,
+                           pvalue_target,
+                           trim_edges,
+                           maximization_target, 
+                           gobig,
+                           random_seed])
+        
+    total_regions = 0
+    total_peaks = 0
+    region_dfs = []
+    crd_pool = multiprocessing.Pool(num_cores)
+    
+    for chrom, regions_found, peaks_found, chrom_df in crd_pool.map(find_crds_worker, params):
+        total_regions += regions_found
+        total_peaks += peaks_found
+        region_dfs.append(chrom_df)
+    crd_pool.close()
+    crd_pool.join()
+    
+    log_print('Found {} CRDs in {} total pairwise correlations.'.format(total_regions, total_peaks), 1)
+    
+    if total_regions > 0:
+        all_regions_df = pandas.concat([region_df for region_df in region_dfs if region_df.shape[0] > 0], axis=0)
+        
+        if fdr is not None:
+            passfail, qvals, _, _  = multipletests(all_regions_df.pval, alpha=fdr, method='fdr_bh')
+            all_regions_df['qval'] = qvals
+            all_regions_df = all_regions_df.loc[passfail]
+            log_print('{} CRDs passed FDR threshold of {}.'.format(all_regions_df.shape[0], fdr), 1)
+        
+        return all_regions_df    
+        
+    else:
+        return None    
+
+def find_crds_worker(params):
+    chrom, annotations, min_score, max_pval, min_size, max_size, edge_weight_power, pvalue_target, trim_edges, maximization_target, gobig, random_seed = params
+    
+    if len(chrom_data[chrom].shape) > 1:
+        assert chrom_data[chrom].shape[0] == chrom_data[chrom].shape[1]
+    num_features = chrom_data[chrom].shape[0]
+
+   
+    this_chrom_regions = find_coupled_peaks_1chrom(chrom,
+                                            min_score=min_score,
+                                            max_pval=max_pval,
+                                            min_size=min_size,
+                                            max_size=max_size,
+                                            maximization_target=maximization_target,
+                                            pvalue_target=pvalue_target,
+                                            edge_weight_power=edge_weight_power,
+                                            trim_edges=trim_edges,
+                                            gobig=gobig,
+                                            random_seed=random_seed)  
+    regions_found = len(this_chrom_regions)
+
+    log_print('{}: extracting annotations ...'.format(chrom), 2)
+
+    chrom_df = generate_bed_df(this_chrom_regions, annotations)
+
+    log_print('{}: done.'.format(chrom), 2)
+    
+    return chrom, regions_found, num_features, chrom_df
+        
--- scripts/peas.py
+++ scripts/peas.py
@@ -1,13 +1,64 @@
 #!python
 
+import sys
 import argparse
 import peas
 
+def validate_columns(col_param_string, mode):
+    column_elements = [col.strip() for col in col_param_string.split(',')]
+    try:
+        column_elements = [int(col) for col in column_elements]
+    except ValueError:
+        print('All elements of COLUMNS must be integers.')
+        return False
+    
+    if not sum([col >= 0 for col in column_elements]) > 0:
+        print ('All elements of COLUMNS must be non-negative.')
+        return False
+        
+    if mode == 'matrix':
+        if len(column_elements) < 3:
+            print('In matrix mode you must specify at least 3 columns to correlate.')
+            return False
+    else:
+        if len(column_elements) not in (1,2):
+            print('In vector mode you must specify either 1 or 2 columns.')
+            return False
+    
+    return True
 
+    
 def main():
-    parser = argparse.ArgumentParser('PEAS')
+    parser = argparse.ArgumentParser(prog='peas')
+    parser.add_argument('mode', help='operate in vector or matrix mode', type=str, choices=('vector', 'matrix'))
+    parser.add_argument('input_file', help='input file', type=argparse.FileType('r'))
+    parser.add_argument('columns', help='which columns (numbered left-right starting at 0) in the BED file to use as score values. In vector mode, either a single column or a pair of columns can be specified. If a pair is specified, the second column in the pair will be subracted from the first column to generate the vector for PEAS analysis. In matrix mode, 3 or more columns must be specified for use in generating correlation matrices.')
+    
+    # Add options for score transformations
+    
+    parser.add_argument('--tail', '-t', help='look for regions with greater than expected values (right), lower than expected values (left) or either (both)', choices=('right', 'left', 'both'), default='both')
+    parser.add_argument('--min-score', '-s', help='minimum region score', type=float, default=0.0)
+    parser.add_argument('--pvalue', '-p', help='p-value threshold', type=float, default=1e-3)
+    parser.add_argument('--fdr', '-f', help='FDR threshold', type=float, default=0.05)
+    parser.add_argument('--min-size', '-m', help='minimum region size', type=int, default=2)
+    parser.add_argument('--max-size', '-n', help='maximum region size', type=int, default=0)
+    parser.add_argument('--alpha', '-a', help='power to apply to edge weights prior to computing optimal regions', type=float, default=2)
+    
+    matrix_options = parser.add_argument_group(title='matrix options', description='options that only apply when operating in matrix mode')
+    matrix_options.add_argument('--parameter-smoothing-size', help='size of window to consider when smoothing piecewise distribution parameters', type=str, default=5)
+    matrix_options.add_argument('--distribution-type', help='what type of piecewise distribution to fit to the permuted data', choices=('pw_power', 'pw_linear'))
+    
+    vector_options = parser.add_argument_group(title='vector options', description='options that only apply when operating in vector mode')
+    vector_options.add_argument('--bins', '-b', help='how many bins to use for approximating the empirical null distributions', type=str, default='auto')
+    
+    args = parser.parse_args()
+    
+    if validate_columns(args.columns, args.mode):
+    
+        print(args)
 
-    peas.main.find_ropes()
+
+    # peas.main.find_ropes()
 
 
 if __name__ == '__main__':
